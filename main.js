e1b6c0669052218f265577f4adf1425365253fb8
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-05-18 16:38:37 +0200

    update

--- main.js
+++ main.js
@@ -3,15 +3,9 @@ const { createApp } = Vue;
 createApp ({
     data( ) {
         return{
-            message : "Hello world!"
+            message : "Hello world!",
+            image: 'https://picsum.photos/id/1005/600/200'
         }
     }
 }).mount("#app");
 
-createApp ({
-    data() {
-        return {
-            image: 'https://picsum.photos/id/1005/600/200'
-        }
-    }
-}).mount("#bonus");
